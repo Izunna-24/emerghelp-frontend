a74bf432ab1b36def79ed242da953fde199a27f8
Izuchukwu Ijeudo <151368567+Izunna-24@users.noreply.github.com>
2024-08-15 12:27:45 +0100

    removed navbar

--- src/component/layout/Layout.jsx
+++ src/component/layout/Layout.jsx
@@ -2,13 +2,11 @@ import {Outlet} from "react-router-dom";
 import Footer from "../footer/Footer";
 import Hero from "../hero/Hero";
 import Emerghelp from "../emerghelp/Emerghelp";
-import Navbar from "../navBar/Navbar";
 
 
 const Layout = () => {
     return(
         <>
-            <Navbar/>
             <Outlet/>
             <Hero/>
             <Emerghelp/>
