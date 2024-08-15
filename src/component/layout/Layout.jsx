95681d4a3064c465346ebc1f462f4129a50f4af9
Izuchukwu Ijeudo <151368567+Izunna-24@users.noreply.github.com>
2024-08-15 12:35:31 +0100

    added navbar

--- src/component/layout/Layout.jsx
+++ src/component/layout/Layout.jsx
@@ -2,11 +2,13 @@ import {Outlet} from "react-router-dom";
 import Footer from "../footer/Footer";
 import Hero from "../hero/Hero";
 import Emerghelp from "../emerghelp/Emerghelp";
+import Navbar from "../navBar/Navbar";
 
 
 const Layout = () => {
     return(
         <>
+            <Navbar/>
             <Outlet/>
             <Hero/>
             <Emerghelp/>
